f1c56a6d35442c0382d018ba32164bcb6467e93e
mattn <mattn.jp@gmail.com>
2010-06-17 15:45:46 +0900

    change example 'treeview' to show gtk's stock icons.

--- example/treeview/treeview.go
+++ example/treeview/treeview.go
@@ -2,41 +2,44 @@ package main
 
 import (
   "os";
+  "glib";
   "gtk";
+  "gdkpixbuf";
   "unsafe";
 )
 
 func main() {
 	gtk.Init(&os.Args);
 	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL);
-	window.SetTitle("TreeView!");
+	window.SetTitle("GTK Stock Icons");
 	window.Connect("destroy", func(w *gtk.GtkWidget, args []unsafe.Pointer) {
 		gtk.MainQuit();
 	}, nil);
 
-	vbox := gtk.VBox(false, 1)
+	swin := gtk.ScrolledWindow(nil, nil);
 
-	store := gtk.ListStore(gtk.TYPE_STRING, gtk.TYPE_BOOL);
+	store := gtk.ListStore(gtk.TYPE_STRING, gtk.TYPE_BOOL, gdkpixbuf.GetGdkPixbufType());
 	treeview := gtk.TreeView();
 	treeview.SetModel(store.ToTreeModel());
 
-	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("text", gtk.CellRendererText(), "text", 0));
-	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("toggle", gtk.CellRendererToggle(), "active", 1));
+	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("name", gtk.CellRendererText(), "text", 0));
+	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("check", gtk.CellRendererToggle(), "active", 1));
+	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("icon", gtk.CellRendererPixbuf(), "pixbuf", 2));
 
-	var iter gtk.GtkTreeIter;
-	store.Append(&iter);
-	store.Set(&iter, "amachang", true)
-	store.Append(&iter);
-	store.Set(&iter, "otsune", false)
-	store.Append(&iter);
-	store.Set(&iter, "miyagawa", true)
-	store.Append(&iter);
-	store.Set(&iter, "tokuhirom", false)
+	swin.Add(treeview);
 
-	vbox.Add(treeview);
-
-	window.Add(vbox);
-	window.SetSizeRequest(200, 200);
+	window.Add(swin);
+	window.SetSizeRequest(400, 200);
 	window.ShowAll();
+
+	n := 0;
+	gtk.GtkStockListIDs().ForEach(func(d interface{}, v interface{}) {
+		id := glib.GPtrToString(d);
+		var iter gtk.GtkTreeIter;
+		store.Append(&iter);
+		store.Set(&iter, id, (n == 1), gtk.Image().RenderIcon(id, gtk.GTK_ICON_SIZE_SMALL_TOOLBAR, "").Pixbuf);
+		n = 1 - n;
+	}, 0)
+
 	gtk.Main();
 }
