eea3d2ade77e9b03f43cf28dc38c37e4845f6949
mattn <mattn.jp@gmail.com>
2010-06-22 17:51:51 +0900

    order

--- example/treeview/treeview.go
+++ example/treeview/treeview.go
@@ -20,13 +20,13 @@ func main() {
 
 	store := gtk.ListStore(gtk.TYPE_STRING, gtk.TYPE_BOOL, gdkpixbuf.GetGdkPixbufType());
 	treeview := gtk.TreeView();
+	swin.Add(treeview);
+
 	treeview.SetModel(store.ToTreeModel());
 
 	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("name", gtk.CellRendererText(), "text", 0));
 	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("check", gtk.CellRendererToggle(), "active", 1));
 	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("icon", gtk.CellRendererPixbuf(), "pixbuf", 2));
-
-	swin.Add(treeview);
 
 	window.Add(swin);
 	window.SetSizeRequest(400, 200);
