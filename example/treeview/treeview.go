cea8ac2f300829b9e4faecd6d106ceab2725c7fa
mattn <mattn.jp@gmail.com>
2010-06-22 17:53:03 +0900

    order

--- example/treeview/treeview.go
+++ example/treeview/treeview.go
@@ -23,15 +23,9 @@ func main() {
 	swin.Add(treeview);
 
 	treeview.SetModel(store.ToTreeModel());
-
 	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("name", gtk.CellRendererText(), "text", 0));
 	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("check", gtk.CellRendererToggle(), "active", 1));
 	treeview.AppendColumn(gtk.TreeViewColumnWithAttributes("icon", gtk.CellRendererPixbuf(), "pixbuf", 2));
-
-	window.Add(swin);
-	window.SetSizeRequest(400, 200);
-	window.ShowAll();
-
 	n := 0;
 	gtk.GtkStockListIDs().ForEach(func(d interface{}, v interface{}) {
 		id := glib.GPtrToString(d);
@@ -40,6 +34,10 @@ func main() {
 		store.Set(&iter, id, (n == 1), gtk.Image().RenderIcon(id, gtk.GTK_ICON_SIZE_SMALL_TOOLBAR, "").Pixbuf);
 		n = 1 - n;
 	}, 0)
+
+	window.Add(swin);
+	window.SetSizeRequest(400, 200);
+	window.ShowAll();
 
 	gtk.Main();
 }
