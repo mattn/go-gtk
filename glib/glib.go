1215b44a24bdb303aec2d417ddd94d40c412ac38
mattn <mattn.jp@gmail.com>
2010-06-25 18:40:07 +0900

    change name of glib.Error/glib.SList.

--- glib/glib.go
+++ glib/glib.go
@@ -5,14 +5,24 @@ package glib
 static GSList* to_slist(void* sl) {
 	return (GSList*)sl;
 }
+static GError* to_error(void* err) {
+	return (GError*)err;
+}
 
 static inline char* to_charptr(gpointer s) { return (char*)s; }
 */
 import "C"
 import "unsafe"
 
+func GPtrToString(p interface{}) string {
+	return C.GoString(C.to_charptr(p.(C.gpointer)))
+}
+
+//-----------------------------------------------------------------------
+// g_slist
+//-----------------------------------------------------------------------
 type SList struct {
-	List *C.GSList
+	GSList *C.GSList
 }
 
 func FromSList(sl unsafe.Pointer) *SList {
@@ -20,71 +30,71 @@ func FromSList(sl unsafe.Pointer) *SList {
 		C.to_slist(sl)}
 }
 func (v SList) ToSList() *C.GSList {
-	return v.List
+	return v.GSList
 }
 func (v SList) Data() interface{} {
-	return v.List.data
+	return v.GSList.data
 }
 func GSListAlloc() *SList {
 	return &SList{C.g_slist_alloc()}
 }
 func (v SList) Free() {
-	C.g_slist_free(v.List)
+	C.g_slist_free(v.GSList)
 }
 func (v SList) Free1() {
-	C.g_slist_free1(v.List)
+	C.g_slist_free1(v.GSList)
 }
 func (v SList) Append(data unsafe.Pointer) *SList {
-	return &SList{C.g_slist_append(v.List, C.gpointer(data))}
+	return &SList{C.g_slist_append(v.GSList, C.gpointer(data))}
 }
 func (v SList) Prepend(data unsafe.Pointer) *SList {
-	return &SList{C.g_slist_prepend(v.List, C.gpointer(data))}
+	return &SList{C.g_slist_prepend(v.GSList, C.gpointer(data))}
 }
 // GSList* g_slist_insert (GSList *list, gpointer data, gint position) G_GNUC_WARN_UNUSED_RESULT;
 // GSList* g_slist_insert_sorted (GSList *list, gpointer data, GCompareFunc func) G_GNUC_WARN_UNUSED_RESULT;
 // GSList* g_slist_insert_sorted_with_data (GSList *list, gpointer data, GCompareDataFunc func, gpointer user_data) G_GNUC_WARN_UNUSED_RESULT;
 func (v SList) InsertBefore(sibling SList, data unsafe.Pointer) *SList {
-	return &SList{C.g_slist_insert_before(v.List, sibling.List, C.gpointer(data))}
+	return &SList{C.g_slist_insert_before(v.GSList, sibling.GSList, C.gpointer(data))}
 }
 func (v SList) Concat(llink SList) *SList {
-	return &SList{C.g_slist_concat(v.List, llink.List)}
+	return &SList{C.g_slist_concat(v.GSList, llink.GSList)}
 }
 func (v SList) Remove(data unsafe.Pointer) *SList {
-	return &SList{C.g_slist_remove(v.List, C.gconstpointer(data))}
+	return &SList{C.g_slist_remove(v.GSList, C.gconstpointer(data))}
 }
 func (v SList) RemoveAll(data unsafe.Pointer) *SList {
-	return &SList{C.g_slist_remove_all(v.List, C.gconstpointer(data))}
+	return &SList{C.g_slist_remove_all(v.GSList, C.gconstpointer(data))}
 }
 func (v SList) RemoveLink(llink SList) *SList {
-	return &SList{C.g_slist_delete_link(v.List, llink.List)}
+	return &SList{C.g_slist_delete_link(v.GSList, llink.GSList)}
 }
 func (v SList) DeleteLink(llink SList) *SList {
-	return &SList{C.g_slist_delete_link(v.List, llink.List)}
+	return &SList{C.g_slist_delete_link(v.GSList, llink.GSList)}
 }
 func (v SList) Reverse() *SList {
-	return &SList{C.g_slist_reverse(v.List)}
+	return &SList{C.g_slist_reverse(v.GSList)}
 }
 func (v SList) Copy() *SList {
-	return &SList{C.g_slist_copy(v.List)}
+	return &SList{C.g_slist_copy(v.GSList)}
 }
 func (v SList) Nth(n uint) *SList {
-	return &SList{C.g_slist_nth(v.List, C.guint(n))}
+	return &SList{C.g_slist_nth(v.GSList, C.guint(n))}
 }
 func (v SList) Find(data unsafe.Pointer) *SList {
-	return &SList{C.g_slist_find(v.List, C.gconstpointer(data))}
+	return &SList{C.g_slist_find(v.GSList, C.gconstpointer(data))}
 }
 // GSList* g_slist_find_custom (GSList *list, gconstpointer data, GCompareFunc func);
 func (v SList) Position(llink SList) int {
-	return int(C.g_slist_position(v.List, llink.List))
+	return int(C.g_slist_position(v.GSList, llink.GSList))
 }
 func (v SList) Index(data unsafe.Pointer) int {
-	return int(C.g_slist_index(v.List, C.gconstpointer(data)))
+	return int(C.g_slist_index(v.GSList, C.gconstpointer(data)))
 }
 func (v SList) Last() *SList {
-	return &SList{C.g_slist_last(v.List)}
+	return &SList{C.g_slist_last(v.GSList)}
 }
 func (v SList) Length() uint {
-	return uint(C.g_slist_length(v.List))
+	return uint(C.g_slist_length(v.GSList))
 }
 func (v SList) ForEach(callback func(interface{}, interface{}), user_data interface{}) {
 	for n := uint(0); n < v.Length(); n++ {
@@ -94,9 +104,17 @@ func (v SList) ForEach(callback func(interface{}, interface{}), user_data interf
 // GSList* g_slist_sort (GSList *list, GCompareFunc compare_func) G_GNUC_WARN_UNUSED_RESULT;
 // GSList* g_slist_sort_with_data (GSList *list, GCompareDataFunc compare_func, gpointer user_data) G_GNUC_WARN_UNUSED_RESULT;
 func (v SList) NthData(n uint) interface{} {
-	return C.g_slist_nth_data(v.List, C.guint(n))
+	return C.g_slist_nth_data(v.GSList, C.guint(n))
 }
 
-func GPtrToString(p interface{}) string {
-	return C.GoString(C.to_charptr(p.(C.gpointer)))
+//-----------------------------------------------------------------------
+// g_error
+//-----------------------------------------------------------------------
+type Error struct {
+	GError *C.GError
+}
+
+func FromError(err unsafe.Pointer) *Error {
+	return &Error{
+		C.to_error(err)}
 }
