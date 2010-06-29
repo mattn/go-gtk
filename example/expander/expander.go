38fa64cdadcb24b0c80625c78753d2ff7ea74093
mattn <mattn.jp@gmail.com>
2010-06-29 16:14:59 +0900

    add example for GtkExpander

---
+++ example/expander/expander.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "os";
+  "gtk";
+)
+
+func main() {
+	gtk.Init(&os.Args);
+	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL);
+	window.SetTitle("GTK Stock Icons");
+	window.Connect("destroy", func() {
+		gtk.MainQuit();
+	}, nil);
+
+	vbox := gtk.VBox(true, 0);
+	vbox.SetBorderWidth(5);
+	expander := gtk.Expander("dan the ...");
+	expander.Add(gtk.Label("404 contents not found"));
+	vbox.PackStart(expander, false, false, 0);
+
+	window.Add(vbox);
+	window.ShowAll();
+
+	gtk.Main();
+}
