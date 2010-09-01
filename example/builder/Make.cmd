8cc963d636e951e9642cd23563480f90fc0432b7
mattn <mattn.jp@gmail.com>
2010-09-01 11:44:24 +0900

    fixed workaround about local install.

---
+++ example/builder/Make.cmd
@@ -0,0 +1,12 @@
+include $(GOROOT)/src/Make.inc
+
+TARG=builder
+GOFILES=builder.go
+CGOFILES=builder.go
+GC=${O}g -Ipkg/$(GOOS)_$(GOARCH)
+
+include $(GOROOT)/src/Make.cmd
+
+installlocal: builder
+	mkdir -p pkg/$(GOOS)_$(GOARCH)
+	mv builder pkg/$(GOOS)_$(GOARCH)/.
