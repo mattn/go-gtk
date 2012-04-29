a27e14df0a9cba73d0037093000cb4958a83b523
vamate223 <vamate@gmail.com>
2012-04-29 08:23:27 +0300

    Update gdk/gdk_windows.go

--- gdk/gdk_windows.go
+++ gdk/gdk_windows.go
@@ -5,8 +5,12 @@ package gdk
 // #cgo pkg-config: gdk-2.0 gthread-2.0
 */
 import "C"
+import ("fmt","strconv","unsafe")
+
 
 func (v *GdkWindow) GetNativeWindowID() int32 {
-	return int32(C.gdk_win32_drawable_get_handle((*C.GdkDrawable)(unsafe.Pointer(v.Window))))
+h := fmt.Sprint(C.gdk_win32_drawable_get_handle((*C.GdkDrawable)(unsafe.Pointer(v.Window))))
+i, _:= strconv.ParseInt(h,10,32)
+return int32(i)
 }
 
