2e5bb90be72659d55946db81e7b8ae47965701af
mattn <mattn.jp@gmail.com>
2012-05-22 08:55:25 +0900

    fix build on linux. close #102

--- gdk/gdk_linux.go
+++ gdk/gdk_linux.go
@@ -5,6 +5,7 @@ package gdk
 // #cgo pkg-config: gdk-2.0 gthread-2.0
 */
 import "C"
+import "unsafe"
 
 func (v *GdkWindow) GetNativeWindowID() int32 {
 	return int32(C.gdk_x11_drawable_get_xid((*C.GdkDrawable)(unsafe.Pointer(v.Window))))
