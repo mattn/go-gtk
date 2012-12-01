9d668256ad33ef3979b4cf49e7d3efaa4ae9509d
Christof Dorner <christof@chdorner.com>
2012-12-01 13:54:49 +0100

    Implement gtk_button_released
    With deprecation notice

--- gtk/gtk.go
+++ gtk/gtk.go
@@ -3010,7 +3010,11 @@ func (v *GtkButton) Pressed() {
 	deprecated_since(2, 20, 0, "gtk_button_pressed()")
 	C.gtk_button_pressed(C.to_GtkButton(v.Widget))
 }
-// gtk_button_released //deprecated since 2.20
+
+func (v *GtkButton) Released() {
+	deprecated_since(2, 20, 0, "gtk_button_released()")
+	C.gtk_button_released(C.to_GtkButton(v.Widget))
+}
 
 func (v *GtkButton) Clicked(onclick interface{}, datas ...interface{}) int {
 	return v.Connect("clicked", onclick, datas...)
