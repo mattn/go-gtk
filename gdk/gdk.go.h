b09f8a580e5ed75e7153e0793f3d61a2725cbb28
Dag Robøle <dag.robole@gmail.com>
2012-12-06 04:44:16 +0100

    Placed C code in separate headers

---
+++ gdk/gdk.go.h
@@ -0,0 +1,27 @@
+#ifndef GO_GDK_H
+#define GO_GDK_H
+
+#include <gdk/gdk.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+
+static gchar* to_gcharptr(char* s) { return (gchar*)s; }
+
+static void free_string(char* s) { free(s); }
+
+static GdkDragContext* to_GtkDragContext(void* l) {
+	return (GdkDragContext*)l;
+}
+
+static void* _gdk_display_get_default() {
+	return (void*) gdk_display_get_default();
+}
+
+static GdkWindow* to_GdkWindow(void* w) {
+	return GDK_WINDOW(w);
+}
+
+#endif
