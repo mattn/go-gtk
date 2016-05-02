5553567f03547d83aa105a74580c6e81adb4c2f7
Andreas Tolfsen <ato@mozilla.com>
2016-05-02 16:56:06 +0100

    build: describe dependency chain for examples
    Following this change examples will not have to be unconditionally
    rebuilt unless their corresponding source file has changed.
    
    Build rules themselves are generalised, meaning goal `foo' will always be
    associated with the source file of the same name including the suffix .go.

--- example/example.mk
+++ example/example.mk
@@ -1,25 +1,59 @@
+EXAMPLES := \
+	example/action/action \
+	example/alignment/alignment \
+	example/builder/builder \
+	example/clipboard/clipboard \
+	example/demo/demo \
+	example/dnd/dnd \
+	example/drawable/drawable \
+	example/event/event \
+	example/expander/expander \
+	example/iconview/iconview \
+	example/listview/listview \
+	example/locale/locale \
+	example/notebook/notebook \
+	example/number/number \
+	example/sourceview/sourceview \
+	example/spinbutton/spinbutton \
+	example/statusicon/statusicon \
+	example/table/table \
+	example/thread/thread \
+	example/toolbar/toolbar \
+	example/treeview/treeview \
+	example/twitterstream/twitterstream \
+	example/twitter/twitter
+
 .PHONY: example
-example:
-	cd example/action && go build -x .
-	cd example/alignment && go build -x .
-	cd example/builder && go build -x .
-	cd example/clipboard && go build -x .
-	cd example/demo && go build -x .
-	cd example/spinbutton && go build -x .
-	cd example/toolbar && go build -x .
-	cd example/dnd && go build -x .
-	cd example/drawable && go build -x .
-	cd example/event && go build -x .
-	cd example/expander && go build -x .
-	cd example/iconview && go build -x .
-	cd example/listview && go build -x .
-	cd example/locale && go build -x .
-	cd example/notebook && go build -x .
-	cd example/number && go build -x .
-	cd example/sourceview && go build -x .
-	cd example/statusicon && go build -x .
-	cd example/table && go build -x .
-	cd example/thread && go build -x .
-	cd example/treeview && go build -x .
-	cd example/twitter && go build -x .
-	cd example/twitterstream && go get && go build -x .
+example: $(EXAMPLES)
+	@true
+
+.PHONY: clean-example
+clean-example:
+	rm -f $(EXAMPLES)
+
+%: %.go
+	cd $(@D) && go build -x
+
+example/action/action: example/action/action.go
+example/alignment/alignment: example/alignment/alignment.go
+example/builder/builder: example/builder/builder.go
+example/clipboard/clipboard: example/clipboard/clipboard.go
+example/demo/demo: example/demo/demo.go
+example/dnd/dnd: example/dnd/dnd.go
+example/drawable/drawable: example/drawable/drawable.go
+example/event/event: example/event/event.go
+example/expander/expander: example/expander/expander.go
+example/iconview/iconview: example/iconview/iconview.go
+example/listview/listview: example/listview/listview.go
+example/locale/locale: example/locale/locale.go
+example/notebook/notebook: example/notebook/notebook.go
+example/number/number: example/number/number.go
+example/sourceview/sourceview: example/sourceview/sourceview.go
+example/spinbutton/spinbutton: example/spinbutton/spinbutton.go
+example/statusicon/statusicon: example/statusicon/statusicon.go
+example/table/table: example/table/table.go
+example/thread/thread: example/thread/thread.go
+example/toolbar/toolbar: example/toolbar/toolbar.go
+example/treeview/treeview: example/treeview/treeview.go
+example/twitterstream/twitterstream: example/twitterstream/twitterstream.go
+example/twitter/twitter: example/twitter/twitter.go
