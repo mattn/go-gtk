a9dbd0e222b226cd65de32730ef75b61be46dcef
Franc[e]sco <lolisamurai@tfwno.gf>
2017-06-19 15:51:22 +0200

    don't use C wrapper for gtk_clipboard_wait_for_contents

--- gtk/gtk.go
+++ gtk/gtk.go
@@ -394,7 +394,7 @@ func (v *Clipboard) Store() {
 }
 
 func (v *Clipboard) WaitForContents(target gdk.Atom) *SelectionData {
-	gsel := C._gtk_clipboard_wait_for_contents(v.GClipboard, unsafe.Pointer(target))
+	gsel := C.gtk_clipboard_wait_for_contents(v.GClipboard, unsafe.Pointer(target))
 	return NewSelectionDataFromNative(unsafe.Pointer(gsel))
 }
 
