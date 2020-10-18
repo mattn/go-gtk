1c0657441e18038d5b602a25075dc3e1045756ca
Mohammadali Gharabakloo <ymohammad1288@yahoo.com>
2020-10-18 20:59:25 +0330

    fix type of buffer.CreateTag input

--- _example/demo/demo.go
+++ _example/demo/demo.go
@@ -262,7 +262,7 @@ func main() {
 	buffer.Insert(&start, "Hello ")
 	buffer.GetEndIter(&end)
 	buffer.Insert(&end, "World!")
-	tag := buffer.CreateTag("bold", map[string]string{
+	tag := buffer.CreateTag("bold", map[string]interface{}{
 		"background": "#FF0000", "weight": "700"})
 	buffer.GetStartIter(&start)
 	buffer.GetEndIter(&end)
